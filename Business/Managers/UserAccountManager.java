13b540d2b6966172cd973f5910ded94389ac13ac
GorrinoChan <daniel.morera1701@gmail.com>
2025-04-23 09:36:06 +0200

    Account Manager y Manager de inicilizzación

--- Business/Managers/UserAccountManager.java
+++ Business/Managers/UserAccountManager.java
@@ -10,16 +10,36 @@ import java.util.List;
 public class UserAccountManager {
 
     AccountDao accountDao;
+    String userName;
 
-    public UserAccountManager() {
+    public UserAccountManager(String userName) {
         this.accountDao = new AccountDao();
+        this.userName = userName;
     }
 
-    public boolean accountUsernameExist(String userName){
+    public boolean accountUsernameExistByUsername(String userName) {
         boolean exist = true;
-        try{
-            List<Account> specificSqlAccount = accountDao.readSpecificAccountOfDb("nameOfUserAccount", userName);
+        List<Account> specificSqlAccount = null;
+        try {
+            specificSqlAccount = accountDao.readSpecificAccountOfDb("nameOfUserAccount", userName);
         } catch (SQLException | FileNotFoundException e) {
+            exist = false;
+        }
+        if (specificSqlAccount == null){
+            exist = false;
+        }
+            return exist;
+    }
+
+    public boolean accountUsernameExistByMail(String mailOfUser) {
+        boolean exist = true;
+        List<Account> specificSqlAccount = null;
+        try {
+            specificSqlAccount = accountDao.readSpecificAccountOfDb("emailOfUserAccount", mailOfUser);
+        } catch (SQLException | FileNotFoundException e) {
+            exist = false;
+        }
+        if (specificSqlAccount == null) {
             exist = false;
         }
         return exist;
@@ -44,10 +64,56 @@ public class UserAccountManager {
         accountDao.insertNewAccountInDb(userName,emailOfTheAccount,password);
     }
 
-    public List<Account> getAllAccountsCanceled () {
+    public List<Account> getAllAccountsIn () {
         try {
             return accountDao.readAllAccountContentInDb();
         }catch (SQLException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean verifyLengthInPassword (String password){
+        boolean length = false;
+        if(password.length() >= 8){
+            length = true;
+        }
+        return length;
+    }
+
+    public boolean verifyIfThereIsMayusInPassword (String password){
+        boolean mayus = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                mayus = true;
+            }
+        }
+        return mayus;
+    }
+
+    public boolean verifyIfThereIsMinusInPassword (String password){
+        boolean minus = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                minus = true;
+            }
+        }
+        return minus;
+    }
+
+    public boolean verifyIfThereIsNumberInPassword (String password){
+        boolean numeric = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                numeric = true;
+            }
+        }
+        return numeric;
+    }
+
+    public void deleteExistingAccount (String userName){
+        try{
+            accountDao.deleteSpecificAccount(userName);
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
