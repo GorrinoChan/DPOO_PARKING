319deadff18f2c8c6bfea4d2200141428cb8a2b8
GorrinoChan <daniel.morera1701@gmail.com>
2025-05-14 08:27:32 +0200

    arreglos

--- Business/Managers/UserAccountManager.java
+++ Business/Managers/UserAccountManager.java
@@ -282,7 +282,6 @@ public class UserAccountManager {
             if(vehicleExist.isEmpty()){
                this.vehicleDao.insertNewVehicleInDb(licensePlate, typeOfVehicle, userName);
                correctAction = true;
-               System.out.println("hola");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
